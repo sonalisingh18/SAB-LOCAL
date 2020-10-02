fb207bf23adef61a66aa91dadadb8e41d7ee90c5
Sonali Singh <sonalisingh2402@gmail.com>
2020-10-02 20:02:37 +0530

    Added registershop and donation form

--- routes/users.js
+++ routes/users.js
@@ -9,5 +9,11 @@ router.get('/formlogin', (req, res) => res.render('formlogin'));
 // Register Page
 router.get('/formregister', (req, res) => res.render('formregister'));
 
+// Donation Page
+router.get('/formdonate', (req, res) => res.render('formdonate'));
+
+// Add Shop Page
+router.get('/formaddshop', (req, res) => res.render('formaddshop'));
+
 
 module.exports = router;
