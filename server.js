b8bd47722aa10f65118918eadcd29695e3f7e8b1
Sonali Singh <sonalisingh2402@gmail.com>
2020-10-02 16:23:13 +0530

    Added server.js,and html,css,js

---
+++ server.js
@@ -0,0 +1,35 @@
+/* Including Express */
+const express = require('express');
+
+/* Firing Express to Include Modules to app */
+const app = express();
+
+/* Including body parser */
+const bodyParser = require('body-parser');
+
+/* Including mongoose for communicating to data base */
+const mongoose = require('mongoose');
+
+/* Adding path */
+const path = require('path');
+
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+/* Setting Templating engine */
+app.set('view engine' , 'ejs');
+
+/* Express body parser */
+app.use(express.urlencoded({ extended: true }));
+
+/* Access to static files */
+app.use(express.static(path.join(__dirname + '/public')));
+
+
+/* Routes */
+app.use('/', require('./routes/users.js'));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=>{
+    console.log(`Server is running at http://localhost:${port}`);
+});
