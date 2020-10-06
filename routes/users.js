0f958b2b88a3b2daa2fc71d0f3311ab1501b7d3c
Sonali Singh <sonalisingh2402@gmail.com>
2020-10-06 19:50:03 +0530

    Connected register/login to database

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,18 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+
+const { forwardAuthenticated } = require('../config/auth.js');
+const { ensureAuthenticated } = require('../config/auth.js');
+
+const User = require('../models/User');
 
 // Index page
 router.get('/' , (req , res) => res.render('index'));
 
 // Login Page
 router.get('/formlogin', (req, res) => res.render('formlogin'));
+router.get('/formlogin', forwardAuthenticated , (req, res) => res.render('formlogin'));
 
 // Register Page
 router.get('/formregister', (req, res) => res.render('formregister'));
@@ -15,5 +23,99 @@ router.get('/formdonate', (req, res) => res.render('formdonate'));
 // Add Shop Page
 router.get('/formaddshop', (req, res) => res.render('formaddshop'));
 
+/***************************** User Registration *****************************/
+router.post('/formregister', (req, res) => {
+    const { name, email, phone, gender, age, password, password2 } = req.body;
+
+    let errors = [];
+
+    // Check required fields
+    if(!name || !email || !phone || !gender || !age || !password || !password2 ){
+        errors.push({msg: 'Please fill in all fields'});
+    }
+
+    // check Passwords Match
+    if(password!=password2)
+    {
+        errors.push({msg:' Passwords do not match'});
+    }
+
+    //check password length
+    if (password.length < 6) {
+        errors.push({ msg: 'Password must be at least 6 characters' });
+    }
+
+    if (errors.length > 0) {
+        res.render('formregister', {
+            errors,
+            name,
+            email,
+            phone,
+            gender,
+            age,
+            password,
+            password2
+        });
+    }
+    else{
+        //Validation pass
+        User.findOne({email: email}).then(user=>{
+            if(user)
+            {
+                //User exists
+                errors.push({msg: 'Email is already registered'});
+                res.render('formregister', {
+                    errors,
+                    name,
+                    email,
+                    phone,
+                    gender,
+                    age,
+                    password,
+                    password2
+                });
+            }
+            else
+            {
+                const newUser = new User({
+                    name,
+                    email,
+                    phone,
+                    gender,
+                    age,
+                    password
+                });
+
+                // Hash Password
+                bcrypt.genSalt(10, (err, salt) =>{
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        // Set password to hashed
+                        newUser.password = hash;
+                        //Save user
+                        newUser.save()
+                        .then(user => {
+                            req.flash(
+                                'success_msg',
+                                'You are now registered and can log in'
+                              );
+                            res.redirect('/formlogin');
+                        })
+                        .catch(err => console.log(err));
+                    });
+                });
+            }
+        });
+    }
+});
+
+/***************************** User Login *****************************/
+router.post('/formlogin', (req, res, next) => {
+    passport.authenticate('local', {
+      successRedirect: '/',
+      failureRedirect: '/formlogin',
+      failureFlash: true
+    })(req, res, next);
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -10,17 +10,62 @@ const bodyParser = require('body-parser');
 /* Including mongoose for communicating to data base */
 const mongoose = require('mongoose');
 
+/* Including passport for authentication */
+const passport = require('passport');
+
+/* Including flash for message display */
+const flash = require('connect-flash');
+
+/* Including express sessions */
+const session = require('express-session');
+
 /* Adding path */
 const path = require('path');
 
 /* Setting Templating engine */
 app.set('view engine' , 'ejs');
 
+/* Passport Config */
+require('./config/passport')(passport);
+
+/* Database config */
+require('dotenv').config();
+const uri = process.env.mongodburl;
+/* Connect to MongoDB */
+mongoose.connect(uri,{ useNewUrlParser: true,useUnifiedTopology: true,useFindAndModify: false});
+
 /* Express body parser */
 app.use(express.urlencoded({ extended: true }));
 
+/* Express session */
+app.use(
+    session({
+      secret: 'secret',
+      resave: true,
+      saveUninitialized: true
+    })
+);
+
+/* Passport middleware */
+app.use(passport.initialize());
+app.use(passport.session());
+
+/* Middleware to connect flash */
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
+
+
 /* Access to static files */
 app.use(express.static(path.join(__dirname + '/public')));
+
+
 
 
 /* Routes */
